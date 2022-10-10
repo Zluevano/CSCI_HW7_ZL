10361519d3f74d97a03cb2b3bd9a4b6bace3006f
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-10-10 00:35:27 +0000

    Finished table format and added comments for HW7

--- .ccls-cache/@home@runner@CSCIHW7ZL/HW7@HW7.cpp
+++ .ccls-cache/@home@runner@CSCIHW7ZL/HW7@HW7.cpp
@@ -1,3 +1,8 @@
+/*
+Programmer: Zachary Luevano
+Date: October 9th, 2022
+Description: Another book problem that focused on solving a math problem using a for loop and arrays. I feel like this problem really cemented what is possible atleast in the mathmatical sense with Arrays. Anyhow this works by having a set array (resistance) being used for later calculations but defined at the start. A later loop of currenty is entered into a for loop for another 5 variables and take the square of each and reinput them into the array. After that it takes these two to create another array (power) through multiplication. In that final loop it outputs a set of variables from each part that is used to create a graph. Outside of the loop near the end it calculates the totals for them as defined in the final loop.
+*/
 #include <iostream>
 #include <cmath>
 #include <iomanip>

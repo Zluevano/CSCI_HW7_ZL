95689bfac58df0850af5921398a3cd5321b924b3
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-10-10 00:12:31 +0000

    HW6 was extremally easy and rather repativehowever it's interesting how you can still function a char input for the last part.

--- .ccls-cache/@home@runner@CSCIHW7ZL/HW6@HW6.cpp
+++ .ccls-cache/@home@runner@CSCIHW7ZL/HW6@HW6.cpp
@@ -1,3 +1,8 @@
+/*
+Programmer: Zachary Luevano
+Date: October 9th, 2022
+Description: This one was a little hard as I had no idea how to find the lowest amount and correleate it with being picked first, some group work was done. However, the functions works by inputing a number of days and is then passed along to another function that has an input SJ based on n times. The first result is always going to be 0 so setting another variable to -1 to get that as the function for the for loop cannot = -1 is used that first value is also saved as MSJ. Further inputs are processed to determine if  they are lower than whatever the value that set the ans to 0 is lower than it, if it isn't it is ignored and repeated for another input, if the input for SJ is lower than the old value of MSJ it saves that i interval for the final answer.
+*/
 #include <iostream>
 using namespace std;
 

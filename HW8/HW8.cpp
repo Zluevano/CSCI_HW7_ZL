64897c204d614928b2dbaa62cc0553adcac12455
zluevano <111832319+Zluevano@users.noreply.github.com>
2022-10-12 05:11:21 +0000

    HW8 had many attempts across other files as debugger does not work with this many folders, I referred to geeksforgeeks and classmates for some of this which I'm assuming is okay given the difficulty of this and the prior d2l post. Included plenty of notes on how this works to my understanding however.

---
+++ HW8/HW8.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include "MyArrayFunctions.h"
+using namespace std;
+
+int main()
+{
+int n;
+int freq;
+
+cout << "Array Size" << endl;
+cin >> n;
+
+cout << endl; //Gap for console
+
+int list[n];
+int counted[n];
+
+
+//Fill Array with values
+cout << "Input values for array:" << endl;
+for (int i = 0; i < n; i++)
+{
+cin >> list[i];
+}
+
+cout << endl; //Gap for console
+
+//Selection function made in class, rewritten so code is shorter
+selectionSort(list, n);
+
+cout << "Now inputting array in order" << endl;
+for (int i = 0; i < n; i++)
+{
+cout << list[i] << " ";
+}
+
+cout << endl; //Gap for console
+
+
+//Set all of counted[i] to false for later occurence
+for (int i = 0; i <n; i++)
+{
+counted[i] = false;
+}
+
+cout << endl; //Gap for console
+  
+cout << "Verify its all 0 that is counted[n]" << endl;
+printArray(counted, n);
+
+cout << endl; //Gap for console
+
+  
+cout << "Now listing them by occurence" << endl;
+for(int i = 0; i < n; i++){ 
+  //Skips if true
+  if(counted[i] == true)
+  continue; // Skips to next iteration that is i+1 this is done to avoid duplicates.
+  
+  // counts frequency, if value for list[i] = the value for list [j] which is the next of i or i+1 
+  freq = 1; // Assume every case where there is a match is = 1 atleast for the start.
+  for (int j = i + 1; j < n; j++){ //Checks next number from original i.
+    //If list[i] lets say i(1) = 1, and that same list but [i+1] so i(2) also = 1 then set counted[j] for (i+1) = 1 for one
+    // occurence and add the freq by 1 which will be associated with list[i] as its outside second for loop.
+    if (list[i] == list[j]){ 
+      counted[j] = true; // Once this is set to 1 or true and the loop repeats it will not repeat a second time as given by the first for loop.
+      freq++;    
+      }  
+    }  
+    cout << endl;
+    cout << list[i]<< " occurs "<< freq << " times "<< endl;
+  } 
+
+  cout << endl;
+  cout << "for the prior counted[j] the first 0 is 1 = 1 for the second, and this sets counted[2] to 1. and so on..." << endl;
+  cout << "This was mainly done to avoid it repeating itself over and over for list[i], keeps track of duplicates." << endl;
+  printArray(counted, n);
+  cout << "Otherwise it looks like this\n" << endl;
+
+for (int i = 0; i <n; i++)
+{
+counted[i] = false;
+}
+  cout << "Now listing them by occurence" << endl;
+for(int i = 0; i < n; i++){ 
+  //Skips if true
+  if(counted[i] == true)
+  continue; // Skips to next iteration that is i+1 this is done to avoid duplicates.
+  
+  // counts frequency, if value for list[i] = the value for list [j] which is the next of i or i+1 
+  freq = 1; // Assume every case where there is a match is = 1 atleast for the start.
+  for (int j = i + 1; j < n; j++){ //Checks next number from original i.
+    //If list[i] lets say i(1) = 1, and that same list but [i+1] so i(2) also = 1 then set counted[j] for (i+1) = 1 for one
+    // occurence and add the freq by 1 which will be associated with list[i] as its outside second for loop.
+    if (list[i] == list[j]){ 
+      freq++;    
+      }  
+    }  
+    cout << endl;
+    cout << list[i]<< " occurs "<< freq << " times "<< endl;
+  } 
+
+}
+//HW8 had many attempts across other files as debugger does not work with this many folders, I referred to geeksforgeeks and classmates for some of this which I'm assuming is okay given the difficulty of this and the prior d2l post. Included plenty of notes on how this works to my understanding however.
---
+++ HW8/MyArrayFunctions.h
@@ -0,0 +1,162 @@
+/****************************************************
+MyArrayFunctions.h contains functions to help process 1-D arrays more easily.
+Many of these functions are also described in Malik Chapter 8. Last Modified by:
+Dr. M Date: 10/22
+******************************************************/
+// To use the functions here this requires....
+#include <iostream>
+using namespace std;
+
+/*****************************************************
+***************** INTEGER ARRAY FUNCTIONS ************
+*****************************************************/
+
+/* This function prints the elements of an integer array as a single row.
+  Parameters: list[] is the array
+              listSize is the size of the array */
+void printArray(const int list[], int listSize) {
+  int index;
+
+  for (index = 0; index < listSize; index++)
+    cout << list[index] << " ";
+
+  cout << endl;
+  return;
+}
+
+/* This function initializes an integer array to be a constant
+  Parameters: list[] is the array
+              listSize is the size of the array
+              myConst is the constant */
+void initializeArray2Const(int list[], int listSize, int myConst) {
+  int index;
+
+  for (index = 0; index < listSize; index++)
+    list[index] = myConst;
+
+  return;
+}
+
+/* This function reads and stores data in an integer array from the console.
+  Parameters: list[] is the size of the array to be filled
+              sizeList is the size of the array */
+void fillArray(int list[], int listSize) {
+
+  cout << "Enter " << listSize << " integers: "; // comment out for kattis
+
+  int index;
+
+  for (index = 0; index < listSize; index++) {
+    cin >> list[index];
+  }
+
+  return;
+}
+
+/* This function finds the sum of all elements in an integer array. 
+  Parameters: list[] is the array
+              listSize is the size of the array */
+int sumArray(const int list[], int listSize){
+  int index;
+  int sum = 0;
+
+  for (index = 0; index<listSize; index++)
+    sum = sum + list[index];
+
+  return sum;
+}
+
+// This finds and returns the index for the larget value in an int array
+// parameters:    list[] is the array
+//                listSize is the size or length of the array
+int indexLargestElement(const int list[],int listSize)
+{
+int maxIndex = 0; //default assumption, assume largest value is in first spot.
+for (int index = 0; index < listSize; index++)
+{
+  if (list[maxIndex] < list[index])
+    maxIndex = index;
+}
+return maxIndex;
+}
+
+// sequentially search through an int array to find the index of a value.
+// parameters:       list[] is the array we want to search
+//                   listSize is the size of the array.
+//                     int 
+int seqSearch(const int list[], int listSize, int searchItem)
+{
+int loc = 0; // start with 1st spot.
+bool found = false; // we have not found it
+  
+while ((loc < listSize) && (!found))
+{
+if ((list[loc]) == searchItem)
+  found = true;
+  else
+    loc++;
+}
+  if (found)
+    return loc;
+  else{
+    cout << "Item not found." << endl;
+    return -1;
+  }
+}
+
+
+// This function copies some or lal elements from one int array to another
+// Parameters:
+// list1[] is the array we want to copy from.
+// list2[] is the array we want to copy to.
+// numOfElements is the total number of elements to be copied.
+// src is the specific starting position in list1.
+// tar is the specific starting position for list2.
+void copyArray(int list1[], int list2[], int src, int tar, int numOfElements)
+{
+  
+for (int index = 0; index < (src + numOfElements); index++)
+  {
+  list2[tar] = list1[index];
+  tar++;
+  }
+  return;
+}
+
+
+// This function sorts a list by size by smallest to largest
+//Parameters:
+// list[] is the array.
+// length is the size of the array (listsize was also used for the same in here).
+// if the first value is larger than the second one, save the index of that first value 
+// 10 - 1 - 5 - 3 - 8, is 10 smaller than 1, smaller than 5 smaller than 3, smaller than 8?
+// Then is 1 smaller than 5, smaller than 3, smaller than 3, smaller than 8?
+// Then is 5 smaller than 3, smaller than 8, etc...
+// When it encounteres one where it is smaller it swaps.
+// At first assume the first spot is the smallest just for the start and then check it.
+void selectionSort(int list[], int length)
+{
+int index;
+int smallestIndex;
+int loc;
+int temp;
+  
+// Length-1 is done because for the last block on an array it has nothing to compare to see line 147 for further reasoning.
+// Increment through main array
+for (index = 0; index < length -1; index++)
+  {
+  smallestIndex = index;
+    //Additional for loop to check next line and compare to first at one earlier line 142.
+    // Increment for new smallest index.
+    for (loc = index + 1; loc < length; loc++)
+      {
+        if (list[loc] < list[smallestIndex])
+        smallestIndex = loc; // Location of new smallest index.
+      }
+    // Make the swap:
+    temp = list[smallestIndex];
+    list[smallestIndex] = list[index];
+    list[index] = temp;
+  }
+return;
+}
